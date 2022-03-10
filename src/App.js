bcd2ba45b533eca8c68eadcaec2bb944d35a78b9
Heidelol <heideloliver@gmail.com>
2022-03-10 17:17:56 -0500

    tarea2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-header">
+        <Card 
+        img = 'https://th.bing.com/th/id/OIP.6HY1kWt52qwYAv-Q14tuvQHaHa?pid=ImgDet&rs=1'
+        producto="Playera Negra"
+        precio="$12.99"
+        tag="#MenClothing"
+        > 
+        </Card>
+        <Card 
+        img="https://th.bing.com/th/id/OIP.hziUmFUTbAgFI7ynEy1qMwHaHa?pid=ImgDet&rs=1"
+        producto="Playera Azul"
+        precio="$14.99"
+        tag="#MenClothing"
+        > 
+        
+        </Card>
+      </main>
     </div>
   );
 }
